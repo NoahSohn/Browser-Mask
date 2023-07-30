af376f02ae5fffb1f77756ef951c29197ded784c
Noah Sohn <gonoah510@gmail.com>
2023-07-29 17:59:00 -0700

    organized config file

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 // Read the config from config.json
 const loadConfig = async () => {
   try {
-    const response = await fetch(chrome.runtime.getURL('config.json'));
+    const response = await fetch(browser.runtime.getURL('config.json'));
     return await response.json();
   } catch (error) {
     console.error('Error loading config:', error);
-    return { browsers: {}, websites: {} };
+    return { browsers: {} };
   }
 };
 
@@ -24,36 +24,37 @@ const isValidWebpage = (url) => {
 
 // Function to update the badge text based on the current tab
 const updateBadgeText = (tabId) => {
-  chrome.tabs.get(tabId, (tab) => {
+  browser.tabs.get(tabId, (tab) => {
     if (!tab || !tab.url) return;
 
     // Check if the tab is a valid webpage
     if (isValidWebpage(tab.url)) {
-      const website = Object.keys(config.websites).find(site => tab.url.includes(site));
-      if (website) {
-        const browserKey = config.websites[website];
-        const browser = config.browsers[browserKey];
-        chrome.browserAction.setBadgeText({ text: browser.badgeText });
-        chrome.browserAction.setTitle({ title: browser.name });
+      const browserData = Object.values(config.browsers).find((browser) => {
+        return browser.websites.some((site) => tab.url.includes(site));
+      });
+
+      if (browserData) {
+        browser.browserAction.setBadgeText({ text: browserData.badgeText });
+        browser.browserAction.setTitle({ title: browserData.name });
       } else {
-        chrome.browserAction.setBadgeText({ text: '' });
-        chrome.browserAction.setTitle({ title: 'Browser Mask' });
+        browser.browserAction.setBadgeText({ text: '' });
+        browser.browserAction.setTitle({ title: 'Browser Mask' });
       }
     } else {
       // If it's not a valid webpage, hide the badge text
-      chrome.browserAction.setBadgeText({ text: '' });
-      chrome.browserAction.setTitle({ title: 'Browser Mask' });
+      browser.browserAction.setBadgeText({ text: '' });
+      browser.browserAction.setTitle({ title: 'Browser Mask' });
     }
   });
 };
 
 // Update badge text when a new tab is activated
-chrome.tabs.onActivated.addListener((activeInfo) => {
+browser.tabs.onActivated.addListener((activeInfo) => {
   updateBadgeText(activeInfo.tabId);
 });
 
 // Update badge text when the page loads in a tab
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
     updateBadgeText(tabId);
   }
@@ -61,13 +62,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Replace the user agent based on the website's URL
 const replaceUserAgent = (details) => {
-  const website = Object.keys(config.websites).find(site => details.url.includes(site));
-  if (website) {
-    const browserKey = config.websites[website];
-    const browser = config.browsers[browserKey];
-    const userAgent = browser.userAgent;
+  const browserData = Object.values(config.browsers).find((browser) => {
+    return browser.websites.some((site) => details.url.includes(site));
+  });
+
+  if (browserData) {
+    const userAgent = browserData.userAgent;
     return {
-      requestHeaders: details.requestHeaders.map(header => {
+      requestHeaders: details.requestHeaders.map((header) => {
         if (header.name.toLowerCase() === 'user-agent') {
           header.value = userAgent;
         }
@@ -80,8 +82,8 @@ const replaceUserAgent = (details) => {
 };
 
 // Listen for web requests and modify the user agent
-chrome.webRequest.onBeforeSendHeaders.addListener(
+browser.webRequest.onBeforeSendHeaders.addListener(
   replaceUserAgent,
-  { urls: ["<all_urls>"] },
-  ["blocking", "requestHeaders"]
+  { urls: ['<all_urls>'] },
+  ['blocking', 'requestHeaders']
   );
